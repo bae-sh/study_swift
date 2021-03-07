2aed1ae4981221e921ad0f85ee9c859454e9b1f2
bae-sh <b5460881@naver.com>
2021-03-07 19:00:37 +0900

    struct, extension static

--- 7.playground/Contents.swift
+++ 7.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-struct Location{
+/*struct Location{
     let x: Int
     let y: Int
 }
@@ -46,3 +46,111 @@ let myLocation = Location(x: 2, y: 2)
 let stores = [store1, store2, store3]
 
 printCloserStore(currentLocation: myLocation, stores: stores)
+
+
+
+
+
+
+struct Lecture: CustomStringConvertible {
+    var description: String {
+        return "Title: \(name), Instructor: \(subject)"
+    }
+    
+    
+    let subject: String;
+    let name: String;
+    let num: Int;
+}
+func printName(name: String, subject: [Lecture]) -> Void{
+    for i in subject{
+        if(i.name==name){
+            print("\(i.subject)");
+        }
+    }
+}
+let teacher1 = Lecture(subject: "math", name: "bae", num: 20);
+let teacher2 = Lecture(subject: "korean", name: "kim", num: 20);
+let teacher3 = Lecture(subject: "english", name: "gun", num: 20);
+
+let teacher = [teacher1, teacher2, teacher3]
+printName(name: "bae", subject: teacher)
+
+print(teacher1)
+
+
+struct Person {
+    var firstName: String {
+        willSet {
+            print("willSet: \(firstName) --> \(newValue)")
+        }
+        didSet {
+            print("didSet: \(oldValue) --> \(firstName)")
+        }
+    }
+    var lastName: String
+    
+    lazy var isPopular: Bool = {
+        if(fullName == "Jay Park"){
+            return true
+        }else{
+            return false;
+        }
+    }()
+    
+    var fullName: String
+}*/
+
+
+//Property 호출시 값을 하나 반환 Method 는 작업을 한다
+
+
+struct Lecture {
+    var title: String
+    var maxStudents: Int = 10;
+    var numOfRegistered: Int = 0;
+    
+    func remainSeats() -> Int{
+        let remainSeats = maxStudents - numOfRegistered;
+        return remainSeats
+    }
+    
+    mutating func register(){// stored property 를 변경하려면 mutating을 사용
+        numOfRegistered += 1;
+    }
+    static func academy() -> String{
+        return "패캠"
+    }
+    static let target: String = "abcd"// lecture에 직접 프로퍼티나 메소드 실행할떄
+}
+
+var lec = Lecture(title: "Ios Basic")
+
+lec.register();
+
+Lecture.target
+Lecture.academy()
+
+struct Math{
+    static func abs(value: Int) -> Int{
+        if(value>0){
+            return value
+        }else{
+            return -value;
+        }
+    }
+}
+
+Math.abs(value: -20)
+
+extension Math{
+    static func sqaure(value: Int) -> Int{
+        return value * value
+    }
+}
+
+extension Int {
+    func sqaure() -> Int{
+        return self * self
+    }
+}
