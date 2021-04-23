1cfbd9467adb4f52a45aab2badb0a5a0045c95e0
bae-sh <b5460881@naver.com>
2021-04-23 16:12:37 +0900

    외부 라이브러리 사용

--- MyNetflix/MyNetflix/SearchViewController.swift
+++ MyNetflix/MyNetflix/SearchViewController.swift
@@ -7,17 +7,60 @@
 //
 
 import UIKit
+import Kingfisher
 
 class SearchViewController: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var resultCollectionView: UICollectionView!
     
+    var movies: [Movie] = []
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
+extension SearchViewController: UICollectionViewDataSource {
+    //몇개 넘어오니??
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    //어떻게 표현할꺼니??
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ResultCell", for: indexPath) as? ResultCell else { return UICollectionViewCell() }
+        
+        let movie = movies[indexPath.item]
+        let url = URL(string: movie.thumbnailPath)!
+        cell.movieThumbnail.kf.setImage(with: url)
+        cell.backgroundColor = .black
+        return cell
+    }
+}
 
+extension SearchViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let movie = movies[indexPath.item]
+        
+        let sb = UIStoryboard(name: "Player", bundle: nil)
+        let vc = sb.instantiateViewController(identifier: "PlayerViewController") as! PlayerViewController
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: false, completion: nil)
+    }
+}
+
+extension SearchViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let margin: CGFloat = 8
+        let itemSpacing: CGFloat = 10
+        
+        let width = (collectionView.bounds.width - margin * 2 - itemSpacing * 2) / 3
+        let height = width  * 10/7
+        return CGSize(width: width, height: height)
+    }
+}
+
+class ResultCell: UICollectionViewCell {
+    @IBOutlet weak var movieThumbnail: UIImageView!
+}
 
 extension SearchViewController: UISearchBarDelegate {
     
@@ -41,6 +84,10 @@ extension SearchViewController: UISearchBarDelegate {
         SearchAPI.search(searchTerm) { movies in
             //CollectionView 로 표현하기
             print("---> 몇개 넘어왔어? \(movies.count), 첫번째 제목: \(movies.first?.title)")
+            DispatchQueue.main.async {
+                self.movies = movies
+                self.resultCollectionView.reloadData()
+            }
         }
         print("--> 검색어: \(searchTerm)")
     }
